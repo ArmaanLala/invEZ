ac5a9f9977f1932c8c0a2761108299d11cf90c3a
Michael Rehman <michael@michaelrehman.com>
2020-02-08 03:59:17 -0500

    Setting up redirection based on authentication

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,36 @@
 const router = require('express').Router();
 const { auth } = require('../server');
 
+router.get('/isauthenticated', (req, res) => {
+	return res.status(200).json({
+		// auth.currentUser is null when no one is logged in
+		// null is falsy in JS
+		isSignedIn: !!auth.currentUser
+	});
+});
+
 router.route('/signup')
-	.post((req, res) => {
-		// TODO: implement
+	.post(async (req, res) => {
+		const { email, password } = req.body;
+		try {
+			await auth.createUserWithEmailAndPassword(email, password);
+		} catch (err) {
+			console.error(err);
+			return res.status(500).json(err);
+		}
+		return res.status(204);
 	});
 
 router.route('/signin')
-	.post((req, res) => {
-		// TODO: implement
+	.post(async (req, res) => {
+		const { email, password } = req.body;
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+		} catch (err) {
+			console.error(err);
+			return res.status(500).send(err);
+		}
+		return res.status(204);
 	});
 
 module.exports = router;
