655d5e6dc1268da00aec785309526cbb4f133006
Armaan Lala <armaanlala@gmail.com>
2020-02-08 15:19:00 -0500

    Added Charts functionality

--- client/src/components/app/Listing.vue
+++ client/src/components/app/Listing.vue
@@ -1,32 +1,104 @@
 <template>
-	<md-card class="md-layout-item md-size-50 md-small-size-75 md-alignment-center-center">
-		<md-card-header>
-			<h3 class="md-title">{{ listing.name }} ({{ listing.ticker }}) [{{ listing.category }}]</h3>
-		</md-card-header>
-		<md-card-content>
-			<canvas></canvas>
-		</md-card-content>
-	</md-card>
+  <md-card  style="width: 400px">
+    <md-card-header>
+      <h3 class="md-title">{{ listing.name }} ({{ listing.ticker }}) [{{ listing.category }}]</h3>
+    </md-card-header>
+    <md-card-content>
+      <line-chart :chart-data="datacollection"></line-chart>
+      <canvas></canvas>
+    </md-card-content>
+  </md-card>
 </template>
 
 <script>
+import LineChart from "./LineChart.js";
+
 export default {
-	name: 'Listing',
-	props: {
-		listing: Object
-	},
-	async created() {
-		try {
-			const resp = await fetch(`/api/blackrock/${this.listing.ticker}`);
-			const data = await resp.json();
-			console.log(data)
-		} catch (err) {
-			console.error(err.message);
-		}
-	}
-}
+  components: {
+    LineChart
+  },
+  data() {
+    return {
+      datacollection: null,
+      data: null,
+      months: [
+        { month: "January" },
+        { month: "Febuary" },
+        { month: "March" },
+        { month: "April" },
+        { month: "May" },
+        { month: "June" },
+        { month: "July" },
+        { month: "August" },
+        { month: "September" },
+        { month: "October" },
+        { month: "November" },
+        { month: "December" }
+      ],
+      num: null,
+      month1: null,
+      month2: null,
+      month3: null
+    };
+  },
+  mounted() {},
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: [this.month1, this.month2, this.month3],
+        datasets: [
+          {
+            label: "Data",
+            backgroundColor: "rgba(0, 0, 0, 0)",
+            borderColor: "#2d9428",
+            lineTension: 0,
+            data: [
+              this.data[21].level,
+              this.data[22].level,
+              this.data[23].level
+            ]
+          }
+        ],
+      };
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    }
+  },
+  name: "Listing",
+  props: {
+    listing: Object
+  },
+  async created() {
+    try {
+      const resp = await fetch(`/api/blackrock/${this.listing.ticker}`);
+      const data = await resp.json();
+      this.data = data;
+      this.num = parseInt(this.data[23].date.substring(5, 7)) - 1;
+      this.month3 = this.months[this.num].month;
+      this.num = this.num - 1;
+      if (this.num == -1) {
+        this.num = 11;
+      }
+      this.month2 = this.months[this.num].month;
+      this.num = this.num - 1;
+      if (this.num == -1) {
+        this.num = 11;
+      }
+      this.month1 = this.months[this.num].month;
+      console.log(
+        this.months[parseInt(this.data[23].date.substring(5, 7))].month
+      );
+      this.fillData();
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+};
 </script>
 
 <style scoped>
-
+.md-card {
+	flex-wrap: wrap
+}
 </style>
