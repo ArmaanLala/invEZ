c7ed03b76d703cb7f8d6e14b4d0f55f489c3fe62
Armaan Lala <armaanlala@gmail.com>
2020-02-08 06:58:34 -0500

    Worked on Navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,9 +1,15 @@
 <template>
 	<div>
+		<md-app >
+      <md-app-toolbar >
+        <span class="md-title"><router-link to="/"><img src="../assets/Logo.png" height="150" width="150"></router-link></span>
+      
 		<nav>
-			<router-link to="/signin">Sign in</router-link>
-			<router-link to="/signup">Sign up</router-link>
+			<router-link to="/signin"><md-button>Sign In</md-button></router-link>
+			<router-link to="/signup"><md-button>Sign Up</md-button></router-link>
 		</nav>
+		</md-app-toolbar>
+		</md-app>
 	</div>
 </template>
 
@@ -13,5 +19,32 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.md-app-toolbar {
+	display: flex;
+	justify-content: space-between;
+	color: red; 
+
+}
+.md-app {
+    max-height: 400px;
+    border: 1px solid rgba(#000, .12);
+  }
+  nav {
+	/* display: flex; */
+
+  }
+
+
+
+
+@import "~vue-material/dist/theme/engine"; // Import the theme engine
+
+@include md-register-theme("default", (
+  primary: #3fffbe, // The primary color of your application
+  accent: #3fffbe // The accent or secondary color
+));
+
+@import "~vue-material/dist/theme/all"; // Apply the theme
+
 </style>
