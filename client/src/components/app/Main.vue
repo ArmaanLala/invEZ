fe171128997b166e942a86aa87e3360fa9ff7f6f
Armaan Lala <armaanlala@gmail.com>
2020-02-08 12:04:19 -0500

    Added new fields to the main page

--- client/src/components/app/Main.vue
+++ client/src/components/app/Main.vue
@@ -1,40 +1,75 @@
 <template>
-	<div v-if="loading">
-		Loading...
-		<!-- TODO: have an animation here -->
-	</div>
-	<section v-else>
-		<ol>
-			<li v-for="listing in listings" :key="listing.ticker">
-				<Listing :listing="listing" />
-			</li>
-		</ol>
-	</section>
+  <div>
+    
+		<div class="md-layout  fields">
+		<md-field style="width:187.33px;">
+      <label for="money">Amount to Invest</label>
+      <md-input v-model="money" name="money" id="money"></md-input>
+    </md-field>
+<br>
+		</div>
+		<div class="md-layout  fields">
+<br>
+	<md-field style="width:187.33px;">
+          <label for="time">Time to Invest</label>
+          <md-select v-model="time" name="time" id="time">
+            <md-option value="1-month">1 Month</md-option>
+            <md-option value="3-month">3 Months</md-option>
+            <md-option value="6-month">6 Months</md-option>
+            <md-option value="9-month">9 Months</md-option>
+            <md-option value="1-year">1 Year</md-option>
+          </md-select>
+        </md-field>
+</div>
+	
+
+    <!-- <div v-if="loading">
+      Loading...
+      TODO: have an animation here
+    </div>  -->
+    <section>
+      <ol>
+        <li v-for="listing in listings" :key="listing.ticker">
+          <Listing :listing="listing" />
+        </li>
+      </ol>
+    </section>
+  </div>
 </template>
 
 <script>
-import Listing from './Listing'
+import Listing from "./Listing";
 
 export default {
-	name: 'Main',
-	components: { Listing },
-	data() {
-		return {
-			isLoading: true,
-			listings: null
-		}
-	},
-	async created() {
-		// TODO: get listings based on user from BlackRock API
-		const resp = await fetch('/api/blackrock/test');
-		const data = await resp.json();
-		this.listings = data;
-	}
-}
+  name: "Main",
+  components: { Listing },
+  data() {
+    return {
+		money: 100,
+		time: '3-month',
+      isLoading: true,
+      listings: null
+    };
+  },
+  async created() {
+    // TODO: get listings based on user from BlackRock API
+    const resp = await fetch("/api/blackrock/test");
+    const data = await resp.json();
+    this.listings = data;
+  }
+};
 </script>
 
 <style scoped>
+.fields{
+	/* margin: auto; */
+	justify-content: center;
+}
+.md-field{
+	/* width: auto; */
+}
 ol {
-	list-style: none;
+  list-style: none;
+  padding: 0;
 }
 </style>
